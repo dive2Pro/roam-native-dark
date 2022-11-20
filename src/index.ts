def4e07437398e3f0552d05657a8e1b388151be6
toyluck <toylucklebo@gmail.com>
2022-11-20 12:09:00 +0800

    feat: setting panel

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import "./style.css";
 
 function onload({ extensionAPI }: { extensionAPI: RoamExtensionAPI }) {
-  extensionAPI.settings.panel.create([
-    {
-      id: "enabled",
-      name: "Modifier",
-      action: {
-        type: "switch",
-        callback(arg) {
-          console.log(arg);
+  extensionAPI.settings.panel.create({
+    tabTitle: "Roam \"Native\" Dark",
+    settings: [
+      {
+        id: "enabled",
+        name: "Mode",
+        action: {
+          type: "select",
+          items: ["dark", "light"],
+          onChange: (evt: string) => {
+            console.log("Select Changed!", evt);
+            if (evt === "light") {
+              document.body.classList.remove("bp3-dark");
+            } else if (evt === "dark") {
+              document.body.classList.add("bp3-dark");
+            } else {
+              
+            }
+          },
         },
       },
-    },
-  ]);
+    ],
+  });
   document.body.classList.add("bp3-dark");
 }
 
